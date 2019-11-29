5076720b936e60c5b5a513dd53fd28300cc30508
geniusserg <danilovsergeydmitrievich@gmail.com>
2019-11-30 02:22:50 +0300

    9 mark alpha v2.0!

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import console
+import core
 import sys
 
 if __name__ == "__main__":
     print("PhoneBook v1.0, Danilov Sergey. 6th Group IAD. ")
-    console.open_db()
+    core.open()
     print("Database has been loaded sucessfully")
-    console.print_help()
+    core.print_help()
     while True:
         print(">>> ", end = '')
         command = input()
         if command == "quit" or command == "q":
             sys.exit(0)
         else:
-            console.execute(command)
+            core.execute(command)
 
 
