ba2a8febb3610ee62464bb7895f127a37e99fc8a
geniusserg <50704370+geniusserg@users.noreply.github.com>
2020-02-13 09:35:59 +0300

    for tests

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import core
 import sys
+import os
 
 if __name__ == "__main__":
     print("LabWork_1_PhoneBook. Danilov Sergey. 6th Group IAD")
+    if (os.environ["TEST_PY"] == 1):
+        core.help()
+        exit(0)
+        
     core.init()
     print("Database has been loaded sucessfully")
     print("Type 'help' to open instruction of all functions of the program or 'quit' to exit. Have fun!")
