a8571e10a3e850f4b422bd08515067af28970b5c
geniusserg <50704370+geniusserg@users.noreply.github.com>
2020-04-17 23:24:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import sys
 
 if __name__ == "__main__":
     print("LabWork_1_PhoneBook. Danilov Sergey. 6th Group IAD")
-    core.init()
+    if (len(sys.argv)>1):
+        if sys.argv[1] != "debug":
+            core.init()
+        else:
+            print("Debug smoke testing only")
     print("Database has been loaded sucessfully")
     print("Type 'help' to open instruction of all functions of the program or 'quit' to exit. Have fun!")
     while True:
         print(">>> ", end = '')
-        command = input()
+        if (if (len(sys.argv)>1):
+            if sys.argv[1] != "debug":
+                command = input()
+            else:
+                print("Debug smoke testing only")
+                command = "quit"
         if command == "quit" or command == "q":
             sys.exit(0)
         else:
